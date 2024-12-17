fcbcd255a17c8529608747ff18308923a673c1df
mdselimme <mdselimdev@gmail.com>
2024-12-17 12:20:09 +0600

    add-donors-information-in-sever

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,8 +12,7 @@ app.use(cors());
 
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://<db_username>:<db_password>@cluster0.dopmx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dopmx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,14 +27,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+
+    const database = client.db("BloodDonateDb");
+    const bloodDonorCollection = database.collection("DonnerDb");
+
+    // Input Blood Donors Data in Database
+    app.post("/donors", async (req, res) => {
+      const donorsData = req.body;
+      const result = await bloodDonorCollection.insertOne(donorsData);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
